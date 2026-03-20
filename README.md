383348b2073423845ffd37af6e349d60a272c213
Mario Østervold <marioostervold@gmail.com>
2026-03-20 10:26:01 +0100

    Begynt på login
    Begynt på login
    Har komt til delen rett før eg skal sammenligne det brukeren har skrevet inn med det som allerede ligger i databasen
    Starta også serveren

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ Husk rir, det er lagt til settlogg
 Lagt til testdata på øvelser til ein push økt. Lagt testdata til bruker, dette er midlertidig
 
 Også begynt på app.js, her har eg lagt ein sti til øvelser, bruker og økt til spesifikk bruker(usikker på om denne funker.)
+
+### 20/3
+Idemyldring funksjonalitet:
+Brukeren skal kunne legge til økter og øvelser. Bruker skal sjå navnet sitt på forskjermen. Bruker skal kunne sjå tidlegare øvelsar for å sjå progresjon. 
+Begynte på eit login system med forms, blei ikkje ferdig pga serverfeil. Uansett strategien er å la brukeren velge om han skal logge på eller registrere ved hjelp av radioknapper. Om bruker logger inn, tar eg passord fra databasen og sammenligner med det brukerene som allerede ligger inne.
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = required("express");
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
@@ -11,13 +11,19 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.static('public'));
 
+app.post("/api/registrer_bruker", express.json(), (req, res) => {
+    const {brukernavn, passord} = req.body
+    db.prepare("INSERT INTO Person (brukernavn, passord) VALUES(?,?)").run(brukernavn, passord);
+})
+
 app.get('/api/ovelser', (req, res) => {
     const rows = db.prepare('SELECT navn, muskel FROM Ovelse').all();
     res.json(rows);
 });
 
-app.get('api/bruker', (req, res) => {
-    const rows = db.prepare("SELECT brukernavn FROM Person");
+app.get('/api/bruker', (req, res) => {
+    console.log("Forsøker å hente info om brukar.");
+    const rows = db.prepare("SELECT brukernavn, passord FROM Person").all();
     res.json(rows);
 })
 
@@ -35,3 +41,35 @@ app.get('api/oekt/:brukernavn', (req, res) => {
 
     res.json(rows);
 });
+
+// Rute som lar oss registrere en ny fjelltur for en person
+app.post('/api/registrer_ovelse', express.json(), (req, res) => {
+    // Henter ut data fra request body (det som klienten har sendt inn)
+    const {navn, muskel} = req.body;
+    // Registrer den nye fjellturen
+    db.prepare('INSERT INTO Ovelse (navn, muskel) VALUES (?, ?)').run(navn, muskel);
+
+    res.status(201).json({ message: 'Fjellturen er registrert!' });
+});
+
+// Åpner en viss port på serveren, og starter serveren
+app.listen(PORT, () => {
+    console.log(`Server kjører på http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+    // const response = await fetch('/api/registrer_tur', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({ brukernavn, fjellnavn, tidspunkt, varighet, beskrivelse })// merk 
+    // });
